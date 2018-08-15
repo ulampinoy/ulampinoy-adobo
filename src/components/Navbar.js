772f62ab6a686e3d535f6b2f1dcb418ef04031f9
Allan Reyes <chef@ulampinoy.com>
2018-08-16 00:44:52 +0200

    Align the auc nav menu to right

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,25 +14,13 @@ const Navbar = () => (
           </figure>
         </Link>
       </div>
-      <div className="navbar-start">
+      <div className="navbar-end">
         <Link className="navbar-item" to="/about">
           About
         </Link>
         <Link className="navbar-item" to="/products">
           Products
         </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
       </div>
     </div>
   </nav>
