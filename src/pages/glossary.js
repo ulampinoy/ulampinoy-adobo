fde4cd01af5d352ee313eed93a4bd44ba85bbd71
Allan Reyes <chef@ulampinoy.com>
2018-09-10 08:58:04 +0200

    Add Glossary

---
+++ src/pages/glossary.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
+
+export default class IndexPage extends React.Component {
+  render() {
+    const { data } = this.props
+    const { edges: posts } = data.allMarkdownRemark
+
+    return <div className="wrapper">
+      <section className="Section">
+        {posts.map(({ node: post }) => (
+          <div className="post-preview" key={post.id}>
+            <div>
+              <h1>
+                <Link to={post.fields.slug}>
+                  {post.frontmatter.title}
+                </Link>
+              </h1>
+            </div>
+          </div>
+        ))}
+      </section>
+    </div>;
+  }
+}
+
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+}
+
+export const pageQuery = graphql`
+  query GlossaryQuery {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] },
+      filter: { frontmatter: { templateKey: { eq: "glossary-post" } }}
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 240)
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            templateKey
+            date(formatString: "MMMM DD, YYYY")
+          }
+        }
+      }
+    }
+  }
+`
---
+++ src/pages/glossary/adobo.md
@@ -0,0 +1,7 @@
+---
+templateKey: glossary-post
+title: Adobo
+date: 2018-09-10T08:30:03.000Z
+---
+
+More than a dish it a way of cooking with the the triad of vinegar, garlic and soy sauce.
