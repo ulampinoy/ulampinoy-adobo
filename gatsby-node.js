f9be23a465f5d7a191116e82b038cfec8c84160a
Allan Reyes <chef@ulampinoy.com>
2018-09-12 00:30:58 +0200

    Revert from previous

--- gatsby-node.js
+++ gatsby-node.js
@@ -26,55 +26,54 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
       }
     }
   `).then(result => {
-      if (result.errors) {
-        result.errors.forEach(e => console.error(e.toString()))
-        return Promise.reject(result.errors)
-      }
+    if (result.errors) {
+      result.errors.forEach(e => console.error(e.toString()))
+      return Promise.reject(result.errors)
+    }
 
-      const posts = result.data.allMarkdownRemark.edges
+    const posts = result.data.allMarkdownRemark.edges
 
-      posts.forEach(edge => {
-        const id = edge.node.id
-        createPage({
-          path: edge.node.fields.slug,
-          tags: edge.node.frontmatter.tags,
-          coverImage: edge.node.frontmatter.coverImage,
-          description: edge.node.frontmatter.description,
-          whetter: edge.node.frontmatter.whetter,
-          component: path.resolve(
-            `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
-          ),
-          // additional data can be passed via context
-          context: {
-            id,
-          },
-        })
-      })
-
-      // Tag pages:
-      let tags = []
-      // Iterate through each post, putting all found tags into `tags`
-      posts.forEach(edge => {
-        if (_.get(edge, `node.frontmatter.tags`)) {
-          tags = tags.concat(edge.node.frontmatter.tags)
-        }
-      })
-      // Eliminate duplicate tags
-      tags = _.uniq(tags)
-
-      // Make tag pages
-      tags.forEach(tag => {
-        const tagPath = `/tags/${_.kebabCase(tag)}/`
-
-        createPage({
-          path: tagPath,
-          component: path.resolve(`src/templates/tags.js`),
-          context: {
-            tag,
-          },
-        })
+    posts.forEach(edge => {
+      const id = edge.node.id
+      createPage({
+        path: edge.node.fields.slug,
+        tags: edge.node.frontmatter.tags,
+        description: edge.node.frontmatter.description,
+        whetter: edge.node.frontmatter.whetter,
+        component: path.resolve(
+          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
+        ),
+        // additional data can be passed via context
+        context: {
+          id,
+        },
       })
     })
+
+    // Tag pages:
+    let tags = []
+    // Iterate through each post, putting all found tags into `tags`
+    posts.forEach(edge => {
+      if (_.get(edge, `node.frontmatter.tags`)) {
+        tags = tags.concat(edge.node.frontmatter.tags)
+      }
+    })
+    // Eliminate duplicate tags
+    tags = _.uniq(tags)
+
+    // Make tag pages
+    tags.forEach(tag => {
+      const tagPath = `/tags/${_.kebabCase(tag)}/`
+
+      createPage({
+        path: tagPath,
+        component: path.resolve(`src/templates/tags.js`),
+        context: {
+          tag,
+        },
+      })
+    })
+  })
 }
 
 exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
