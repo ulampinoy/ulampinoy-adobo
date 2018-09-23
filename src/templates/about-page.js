3204c9e02423d7819f34b40c6813ffbfe129821b
Allan Reyes <chef@ulampinoy.com>
2018-09-24 01:12:40 +0200

    Add a hidden input to fix the form as per Netlify docs suggestions

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Content, { HTMLContent } from '../components/Content'
+import React from "react";
+import PropTypes from "prop-types";
+import Content, { HTMLContent } from "../components/Content";
 
-export const AboutPageTemplate = ({ title, description, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
+export const AboutPageTemplate = ({
+  title,
+  description,
+  content,
+  contentComponent
+}) => {
+  const PageContent = contentComponent || Content;
 
-  return <div className="wrapper">
+  return (
+    <div className="wrapper">
       <section>
         <div className="gridPost">
           <article>
@@ -18,36 +24,45 @@ export const AboutPageTemplate = ({ title, description, content, contentComponen
       </section>
       <section className="Section">
         <h2>Makipag-ugnayan sa Amin</h2>
-        <form className="contact-form" name="contact" method="POST" netlify>
-          <div>
-            <label>Pangalan:</label>
-            <input type="text" name="name" />
-          </div>
-          <div>
-          <label>Email: </label>
-          <input type="email" name="email" />
-          </div>
-          <div>
-            <label>Mensahe:</label>
-            <textarea name="message" rows="9" />
-          </div>
-          <div>
-            <button type="submit">I-send</button>
+        <form
+          name="contact"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <div className="contact-form">
+            <div>
+              <label>Pangalan:</label>
+              <input type="text" name="name" />
+            </div>
+            <div>
+              <label>Email: </label>
+              <input type="email" name="email" />
+            </div>
+            <div>
+              <label>Mensahe:</label>
+              <textarea name="message" rows="9" />
+            </div>
+            <div>
+              <button type="submit">I-send</button>
+            </div>
           </div>
         </form>
       </section>
-    </div>;
-}
+    </div>
+  );
+};
 
 AboutPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string,
   content: PropTypes.string,
-  contentComponent: PropTypes.func,
-}
+  contentComponent: PropTypes.func
+};
 
 const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
   return (
     <AboutPageTemplate
@@ -56,14 +71,14 @@ const AboutPage = ({ data }) => {
       description={post.frontmatter.description}
       content={post.html}
     />
-  )
-}
+  );
+};
 
 AboutPage.propTypes = {
-  data: PropTypes.object.isRequired,
-}
+  data: PropTypes.object.isRequired
+};
 
-export default AboutPage
+export default AboutPage;
 
 export const aboutPageQuery = graphql`
   query AboutPage($id: String!) {
@@ -75,4 +90,4 @@ export const aboutPageQuery = graphql`
       }
     }
   }
-`
+`;
