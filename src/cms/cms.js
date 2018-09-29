8374e0c67a71d01f22f2f31c8048d4336e67ab15
Allan Reyes <chef@ulampinoy.com>
2018-09-29 08:34:51 +0200

    Fix duplicate name on cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,12 +3,12 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import VideoPostPreview from './preview-templates/VideoPostPreview'
-import BlogPostPreview from './preview-templates/GlossaryPostPreview'
+import GlossaryPostPreview from './preview-templates/GlossaryPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('videos', VideoPostPreview)
 CMS.registerPreviewTemplate('glossary', GlossaryPostPreview)
+CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('products', ProductPagePreview)
