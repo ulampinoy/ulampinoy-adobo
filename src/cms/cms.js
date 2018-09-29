44f1d9c096185578b877b85a3bce1b6fd7489557
Allan Reyes <chef@ulampinoy.com>
2018-09-29 08:31:40 +0200

    Add glossary to Netlify CMS

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,6 +3,7 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import VideoPostPreview from './preview-templates/VideoPostPreview'
+import BlogPostPreview from './preview-templates/GlossaryPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
@@ -10,3 +11,4 @@ CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('videos', VideoPostPreview)
+CMS.registerPreviewTemplate('glossary', GlossaryPostPreview)
