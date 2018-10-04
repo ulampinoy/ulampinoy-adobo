54f9e5eb1dfb4681d22d5caabeaed4e59441c239
Allan Reyes <chef@ulampinoy.com>
2018-10-04 09:13:24 +0200

    Update cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,11 +4,9 @@ import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import VideoPostPreview from './preview-templates/VideoPostPreview'
 import GlossaryPostPreview from './preview-templates/GlossaryPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('videos', VideoPostPreview)
 CMS.registerPreviewTemplate('glossary', GlossaryPostPreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
